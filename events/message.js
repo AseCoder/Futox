8cd3e8590879ff58b05e65682a69b2b56d3d36ca
AseCoder <atte.elo05@gmail.com>
2019-12-04 16:28:37 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -3,6 +3,12 @@ module.exports = {
     if (!msg.content.startsWith(global.Client.defPrefix)) return;
     const cmdName = msg.content.slice(global.Client.defPrefix.length).split(' ')[0];
     if (!global.Client.commands.has(cmdName)) return;
-    global.Client.commands.get(cmdName).run(msg);
+    if (!msg.member.hasPermission('SEND_MESSAGES')) return;
+    if (!msg.member.hasPermission('EMBED_LINKS')) return msg.channel.send('I need permission to Embed Links in order to function properly.');
+    try {
+      global.Client.commands.get(cmdName).run(msg);
+    } catch (error) {
+      msg.channel.send('Ah buggers, something\'s gone wrong! Don\'t panic, i\'ll tell the developers to fix it.');
+    }
   },
 };
