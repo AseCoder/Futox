f7e8bbfd4eed9b7dec15c41624a326f52c07c704
AseCoder <atte.elo05@gmail.com>
2019-12-06 16:21:13 +0200

    update

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -3,8 +3,14 @@ module.exports = {
   run: async (msg, args, cmdName) => {
     if (!args[0]) return global.incorrectUsage(cmdName, msg.channel.id);
     const user = await global.findMember(args[0], msg.guild.id) || await global.getUser(args[0]);
-    console.log(user.tag);
     const embed = new global.Embed();
-    embed.setTitle();
+    embed.setTitle(`Information About ${user.tag || user.user.tag}:`);
+    if (user.user) {
+      embed.setDescription(`:small_orange_diamond: Server Information:\n❯ Nickname: ${user.nickname || 'None'}\n❯ Roles: ${user.roles.array().sort((a, b) => {
+        return b.position - a.position;
+      }).slice(0, -1).map(x => `<@&${x.id}>`).join(' ')}`);
+    }
+
+    msg.channel.send(embed);
   },
 };
--- functions/findMember.js
+++ functions/findMember.js
@@ -1,5 +1,9 @@
 module.exports = {
-  run: async (query, guildID) => {
-    return null;
+  run: (query, guildID) => {
+    return new Promise(async (res, rej) => {
+      const guildMembers = await global.Client.guilds.get(guildID).members.fetch();
+      const member = guildMembers.find(x => x.displayName.toLowerCase().includes(query.toLowerCase()) || x.user.tag.toLowerCase().includes(query.toLowerCase()) || x.user.id.includes(query));
+      res(member);
+    });
   },
 };
